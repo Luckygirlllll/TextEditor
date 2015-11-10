fbef6668655c48adc3c1cd2da66ca24a15b47ea5
Iryna <irynakanivets@gmail.com>
2015-11-10 23:02:36 +0200

    This is simple TextEditor.

---
+++ src/textgen/MyLinkedList.java
@@ -0,0 +1,176 @@
+
+
+package textgen;
+import java.util.AbstractList;
+/** A class that implements a doubly linked list
+*
+* @author UC San Diego Intermediate Programming MOOC team
+*
+* @param <E> The type of the elements stored in the list
+*/
+public class MyLinkedList<E> extends AbstractList<E> {
+   LLNode<E> head;
+   LLNode<E> tail;
+   int size;
+
+   /** Create a new empty LinkedList */
+   public MyLinkedList() {
+       size=0;
+       head=new LLNode<E>(null);
+       tail=new LLNode<E>(null);
+       head.next=tail;
+       tail.prev=head;
+       // TODO: Implement this method
+   }
+
+
+   private void checkBoundsInclusive(int index)
+   {
+       if (index < 0 || index > size)
+           throw new IndexOutOfBoundsException("Index: " + index + ", Size:"+ size);
+   }
+
+   /**
+    * Checks that the index is in the range of existing elements (exclusive).
+    *
+    * @param index the index to check
+    * @throws IndexOutOfBoundsException if index &lt; 0 || index &gt;= size
+    */
+   private void checkBoundsExclusive(int index)
+   {
+       if (index < 0 || index >= size)
+           throw new IndexOutOfBoundsException("Index: " + index + ", Size:"+ size);
+   }
+
+   private void addLastEntry(LLNode<E> e)
+      {
+            modCount++;
+            if (size == 0)
+                head = tail = e;
+           else
+              {
+                    e.prev = tail;
+                    tail.next = e;
+                    tail = e;
+                  }
+            size++;
+          }
+   /**
+    * Appends an element to the end of the list
+    * @param element The element to add
+    */
+   public boolean add(E element )
+   {
+       //create new node here
+       //тут явно должно быть не n, а что-то другое!
+    //   n.next=head.next;
+    //   n.prev=n.next.prev;
+    //   n.next.prev=n;
+    //   head.next=n;
+    //   size++;
+       // TODO: Implement this method
+       	return false;
+   }
+
+   /** Get the element at position index
+    * @throws IndexOutOfBoundsException if the index is out of bounds. */
+   public E get(int index)
+   {
+    //   checkBoundsExclusive(index);
+     //  return get(index).data;
+
+
+       // TODO: Implement this method.
+       	return null;
+   }
+
+   /**
+    // Add an element to the list at the specified index
+    //@param  index where the element should be added
+    // @param element The element to add
+    */
+   public void add(int index, E element )
+   {
+       checkBoundsInclusive(index);
+       LLNode<E> e = new LLNode<E>(element);
+
+       if (index < size)
+       {
+           modCount++;
+           LLNode<E> after = (LLNode<E>) get(index);
+           e.next = after;
+           e.prev = after.prev;
+           if (after.prev == null){
+               head = e;}
+           else
+           { after.prev.next = e;
+           after.prev = e;
+           size++;}
+       }
+       else {
+           addLastEntry(e);
+       }
+       // TODO: Implement this method
+   }
+
+
+   /** Return the size of the list */
+   public int size()
+   {
+       return size;
+       // TODO: Implement this method
+      // return -1;
+   }
+
+   /** Remove a node at the specified index and return its data element.
+    * @param index The index of the element to remove
+    * @return The data element removed
+    * @throws IndexOutOfBoundsException If index is outside the bounds of the list
+    *
+    */
+   public E remove(int index)
+   {
+       checkBoundsExclusive(index);
+       LLNode<E> e =(LLNode<E>) get(index);
+       remove(e);
+       return e.data;
+       // TODO: Implement this method
+       //	return null;
+   }
+
+   /**
+    * Set an index position in the list to a new element
+    * @param index The index of the element to change
+    * @param element The new element
+    * @return The element that was replaced
+    * @throws IndexOutOfBoundsException if the index is out of bounds.
+    */
+   public E set(int index, E element)
+   {
+       checkBoundsExclusive(index);
+       LLNode<E> e =(LLNode<E>) get(index);
+       E old = e.data;
+       e.data = element;
+       return old;
+       // TODO: Implement this method
+       //	return null;
+   }
+}
+
+class LLNode<E>
+{
+   LLNode<E> prev;
+   LLNode<E> next;
+   E data;
+
+   // TODO: Add any other methods you think are useful here
+   // E.g. you might want to add another constructor
+
+   public LLNode(E e)
+   {
+       this.data = e;
+       this.prev = null;
+       this.next = null;
+   }
+
+}
